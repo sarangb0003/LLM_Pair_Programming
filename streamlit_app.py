30993d4675871e4ee469e9988e61a95dc6c354ad
Sarang Bagul <61322867+sarangb0003@users.noreply.github.com>
2023-09-28 21:52:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,9 @@ palm.configure(api_key=user_api_key, transport="rest")
 
 st.sidebar.subheader('Created by: **_Sarang Bagul_**')
 
+st.title('Welcome to LLM Pair Programming :desktop_computer:')
+st.title('Select your coding operation:')
+
 radio = st.radio("Select your operation",["Improve existing code", "Simplify Code", "Write Test Cases",
                                              "Make Code More efficient", "Debug your Code"])
 

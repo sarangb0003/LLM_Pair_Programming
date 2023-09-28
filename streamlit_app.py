aa5b34bdf6b1bb4a67c856cc5f793f7a821f0d74
Sarang Bagul <61322867+sarangb0003@users.noreply.github.com>
2023-09-28 23:02:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ if user_api_key:
     elif radio == "Simplify Code":
         # Prompt for Scenario 2: Simplify code
         prompt_template = """
-        Can you please simplify this code in Python? Write it with list of imported libraries if needed \n
+        Can you please simplify this code in Python? \n
         You are an expert in Pythonic code. 
     
         {question}

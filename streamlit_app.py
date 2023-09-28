8b9b5b8bdb0ff66a1971b2d239e3fc46cbe6fb49
Sarang Bagul <61322867+sarangb0003@users.noreply.github.com>
2023-09-28 22:09:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,77 +21,77 @@ st.header('Welcome to LLM Pair Programming :desktop_computer:')
 
 radio = st.radio('Select your coding operation:', ["Improve existing code", "Simplify Code", "Write Test Cases",
                                              "Make Code More efficient", "Debug your Code"])
-
-# Pick the model that generates text
-models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
-model_bison = models[0]
-
-# Helper function to call the PaLM API
-from google.api_core import retry
-@retry.Retry()
-def generate_text(prompt, model=model_bison, temperature=0.0):
-    return palm.generate_text(prompt=prompt, model=model, temperature=temperature)
-
-if radio == "Improve existing code":
-    # Prompt for Scenario 1: Improve existing code
-    prompt_template = """
-    I don't think this code is the best way to do it in Python, can you help me?
-
-    {question}
-
-    Please explore multiple ways of solving the problem, 
-    and tell me which is the most Pythonic
-    """
-
-elif radio == "Simplify Code":
-    # Prompt for Scenario 2: Simplify code
-    prompt_template = """
-    Can you please simplify this code in Python? \n
-    You are an expert in Pythonic code.
-
-    {question}
-
-    Please comment each line in detail, \n
-    and explain in detail what you did to modify it, and why.
-    """
-
-elif radio == "Write Test Cases":
-    # Prompt for Scenario 3: Write test cases
-    prompt_template = """
-    Can you please create test cases in code for this Python code?
-
-    {question}
-
-    Explain in detail what these test cases are designed to achieve.
-    """
-
-elif radio == "Make Code More efficient":
-    # Prompt for Scenario 4: Make code more efficient
-    prompt_template = """
-    Can you please make this code more efficient?
-
-    {question}
-
-    Explain in detail what you changed and why.
-    """
-
-else:
-    # Prompt for Scenario 5: Debug your code
-    prompt_template = """
-    Can you please help me to debug this code?
-
-    {question}
-
-    Explain in detail what you found and why it was a bug in pythonic way.
-    """
-
-question = """ """
-txt = st.text_area("Paste your code:", question, height=200)
-
-# Complete Answer
-completion = generate_text(
-    prompt = prompt_template.format(question=txt)
-)
-
-if st.button("Result"):
-    st.write(completion.result)
+if user_api_key:
+    # Pick the model that generates text
+    models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+    model_bison = models[0]
+    
+    # Helper function to call the PaLM API
+    from google.api_core import retry
+    @retry.Retry()
+    def generate_text(prompt, model=model_bison, temperature=0.0):
+        return palm.generate_text(prompt=prompt, model=model, temperature=temperature)
+    
+    if radio == "Improve existing code":
+        # Prompt for Scenario 1: Improve existing code
+        prompt_template = """
+        I don't think this code is the best way to do it in Python, can you help me?
+    
+        {question}
+    
+        Please explore multiple ways of solving the problem, 
+        and tell me which is the most Pythonic
+        """
+    
+    elif radio == "Simplify Code":
+        # Prompt for Scenario 2: Simplify code
+        prompt_template = """
+        Can you please simplify this code in Python? \n
+        You are an expert in Pythonic code.
+    
+        {question}
+    
+        Please comment each line in detail, \n
+        and explain in detail what you did to modify it, and why.
+        """
+    
+    elif radio == "Write Test Cases":
+        # Prompt for Scenario 3: Write test cases
+        prompt_template = """
+        Can you please create test cases in code for this Python code?
+    
+        {question}
+    
+        Explain in detail what these test cases are designed to achieve.
+        """
+    
+    elif radio == "Make Code More efficient":
+        # Prompt for Scenario 4: Make code more efficient
+        prompt_template = """
+        Can you please make this code more efficient?
+    
+        {question}
+    
+        Explain in detail what you changed and why.
+        """
+    
+    else:
+        # Prompt for Scenario 5: Debug your code
+        prompt_template = """
+        Can you please help me to debug this code?
+    
+        {question}
+    
+        Explain in detail what you found and why it was a bug in pythonic way.
+        """
+    
+    question = """ """
+    txt = st.text_area("Paste your code:", question, height=200)
+    
+    # Complete Answer
+    completion = generate_text(
+        prompt = prompt_template.format(question=txt)
+    )
+    
+    if st.button("Result"):
+        st.write(completion.result)
